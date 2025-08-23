fe1c29202c05ec774eb26f32b9b66e09ceec5749
SahinShazi <rksahinrone@gmail.com>
2025-08-23 09:26:55 +0600

    update 11-Math.js

---
+++ 11-Math.js
@@ -0,0 +1,25 @@
+//JavaScript Match 
+let num = 30.9900889880;
+let num2 = 2;
+//if need Round Number 
+console.log(Math.round(num)); //31
+console.log(Math.ceil(num)); //31
+console.log(Math.floor(num)); //30
+
+//Squr value 
+console.log(Math.pow(4,3));
+
+//Root Value 
+console.log(Math.abs(-8));
+
+//Minimum value 
+console.log(Math.min(1,2,3,4,5,6,7));
+//Maximum Value 
+console.log(Math.max(1,2,3,4,5,6,7));
+//π Value 
+console.log(Math.PI);
+
+//Sin
+console.log(Math.sin((90 * Math.PI) / 180));
+//Random Number 
+console.log(Math.floor(Math.random().toFixed(2) * 20 + 1));
