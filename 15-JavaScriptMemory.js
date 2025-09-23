338c95606b674e09472f5d71dafa33f98c9236c0
SahinShazi <rksahinrone@gmail.com>
2025-09-23 09:09:18 +0600

    update 15-JavaScriptMemory.js

---
+++ 15-JavaScriptMemory.js
@@ -0,0 +1,37 @@
+//JavaScript Memory Management
+
+/*
+-----------JavaScript Engine-------------
+----1. Call Steak - Steak Save 
+       * Primitive Data Type
+         * String
+         * Number 
+         * Boolean
+         * Undefined
+         * Null 
+         * Symbol
+         * BigInt
+
+Example:
+let fullName = "Sahin"
+let webDeveloper = fullName;
+webDeveloper = "Enam";
+
+
+----2. Heap - Deferent Deferent Save
+       * None Primitive Data Type
+         * Object 
+         * Array
+
+Example:
+let student = {
+name: "Sahin",
+age: 30
+}
+
+let people = student;
+
+people.name = "Enam";
+
+console.log(student);
+*/
